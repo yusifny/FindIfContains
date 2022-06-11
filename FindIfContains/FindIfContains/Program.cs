11260631e4b4b27bb4c23afc748eeb9cdadffad3
Yusif Nazarbayov (CodeAcademy) <99797435+yusifny@users.noreply.github.com>
2022-06-11 22:20:30 +0400

    uploaded second time

---
+++ FindIfContains/FindIfContains/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+namespace FindIfContains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "azərbaycanlılaşdırılmamalılardansınızmışcasına";
+            char searchLetter = 'a';
+            int count = 0;
+            foreach (var let in word)
+            {
+                if (let == searchLetter)
+                {
+                    count++;
+                }
+            }
+            WriteLine(count);
+        }
+    }
+}
